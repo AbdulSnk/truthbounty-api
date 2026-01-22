485fbd9c69e15cff5da48364890877935b494e9e
portable <emmanueltemitopedorcas20@gmail.com>
2026-01-22 10:56:22 +0100

    sync rewards

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RewardsModule } from './rewards/rewards.module';
+import blockchainConfig from './config/blockchain.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [blockchainConfig],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: false, // Use migrations in production
+    }),
+    RewardsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
